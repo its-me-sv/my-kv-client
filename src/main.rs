32d551f2a996fb71fa265fe67d467c2868b08715
Suraj Vijay <surajcpp115@gmail.com>
2023-06-30 12:54:28 +0530

    added utc timestamp to logs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use chrono::Utc;
 use std::io::{stdin, Read, Write};
 use std::net::TcpStream;
 
@@ -5,8 +6,12 @@ const ADDRESS: &str = "127.0.0.1:8080";
 
 fn main() {
     let mut stream = TcpStream::connect(ADDRESS)
-        .unwrap_or_else(|_| panic!("[CLIENT] Couldn't bind to {ADDRESS}"));
-    println!("[CLIENT] Connected to server binded at {ADDRESS}");
+        .unwrap_or_else(|_| panic!("[CLIENT][{}] Couldn't bind to {}", Utc::now(), ADDRESS));
+    println!(
+        "[CLIENT][{}] Connected to server binded at {}",
+        Utc::now(),
+        ADDRESS
+    );
 
     loop {
         let mut input = String::new();
@@ -19,14 +24,18 @@ fn main() {
         match stream.read(&mut buffer) {
             Ok(size) => {
                 if size == 0 {
-                    println!("[Client] Server closed connection");
+                    println!("[Client][{}] Server closed connection", Utc::now());
                     break;
                 }
                 let response = String::from_utf8_lossy(&buffer[..size]).to_string();
-                println!("[CLIENT] Received - {response}");
+                println!("[CLIENT][{}] Received - {}", Utc::now(), response);
             }
             Err(e) => {
-                eprintln!("[CLIENT] Error: {e} | Read error occurred");
+                eprintln!(
+                    "[CLIENT][{}] Error: {} | Read error occurred",
+                    Utc::now(),
+                    e
+                );
                 break;
             }
         }
