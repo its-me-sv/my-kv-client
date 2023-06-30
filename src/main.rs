7cfe91ff79077a302216142ff7e268e9382395b3
Suraj Vijay <surajcpp115@gmail.com>
2023-06-30 12:41:05 +0530

    connecting to server and reading echo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::io::{stdin, Read, Write};
+use std::net::TcpStream;
+
+const ADDRESS: &str = "127.0.0.1:8080";
+
 fn main() {
-    println!("Hello, world!");
+    let mut stream = TcpStream::connect(ADDRESS)
+        .unwrap_or_else(|_| panic!("[CLIENT] Couldn't bind to {ADDRESS}"));
+    println!("[CLIENT] Connected to server binded at {ADDRESS}");
+
+    loop {
+        let mut input = String::new();
+        println!("Input:");
+        stdin().read_line(&mut input).expect("Failed to read input");
+
+        stream.write_all(input.trim().as_bytes()).unwrap();
+
+        let mut buffer = [0; 1024];
+        match stream.read(&mut buffer) {
+            Ok(size) => {
+                if size == 0 {
+                    println!("[Client] Server closed connection");
+                    break;
+                }
+                let response = String::from_utf8_lossy(&buffer[..size]).to_string();
+                println!("[CLIENT] Received - {response}");
+            }
+            Err(e) => {
+                eprintln!("[CLIENT] Error: {e} | Read error occurred");
+                break;
+            }
+        }
+    }
 }
