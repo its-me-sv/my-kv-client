e24bc574c37e5a0dac02e7128f4c08a3589d3e34
Suraj Vijay <surajcpp115@gmail.com>
2023-07-04 12:45:59 +0530

    removed tokio runtime

--- src/lib/utils/client.rs
+++ src/lib/utils/client.rs
@@ -1,13 +1,27 @@
-use tokio::{io::AsyncWriteExt, net::TcpStream};
+use std::io::{Read, Write};
+use std::net::TcpStream;
 
 use super::log::{log, Log};
 
-pub async fn spawn_client(address: &str) -> TcpStream {
+pub fn spawn_client(address: &str) -> TcpStream {
     TcpStream::connect(address)
-        .await
         .unwrap_or_else(|_| panic!("{}", log(Log::ClientConnectIssue(address.to_string()))))
 }
 
-pub async fn write_to_server(client: &mut TcpStream, content: impl Into<String>) {
-    client.write_all(content.into().as_bytes()).await.unwrap();
+pub fn write_to_server(client: &mut TcpStream, content: impl Into<String>) {
+    client.write_all(content.into().as_bytes()).unwrap();
+}
+
+pub fn read_from_server(client: &mut TcpStream) -> Log {
+    let mut buffer = [0; 1024];
+    match client.read(&mut buffer) {
+        Ok(size) => {
+            if size == 0 {
+                return Log::ServerClosedConnection;
+            }
+            let response = String::from_utf8_lossy(&buffer[..size]).to_string();
+            Log::ServerResponse(response)
+        }
+        Err(e) => Log::ReadError(e.to_string()),
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
 use client::utils::{
-    client::{spawn_client, write_to_server},
+    client::{read_from_server, spawn_client, write_to_server},
     input::get_input,
     log::{log, Log},
 };
-use tokio::io::AsyncReadExt;
 
-#[tokio::main]
-async fn main() {
+fn main() {
     let address = "127.0.0.1:8080";
-    let mut client = spawn_client(address).await;
+    let mut client = spawn_client(address);
 
     println!(
         "{}",
@@ -17,22 +15,14 @@ async fn main() {
 
     loop {
         let user_input = get_input();
-        write_to_server(&mut client, user_input).await;
+        write_to_server(&mut client, user_input);
 
-        let mut buffer = [0; 1024];
-        match client.read(&mut buffer).await {
-            Ok(size) => {
-                if size == 0 {
-                    println!("{}", log(Log::ServerClosedConnection));
-                    break;
-                }
-                let response = String::from_utf8_lossy(&buffer[..size]).to_string();
-                log(Log::ServerResponse(response));
-            }
-            Err(e) => {
-                eprintln!("{}", log(Log::ReadError(e.to_string())));
+        match read_from_server(&mut client) {
+            Log::ServerClosedConnection => {
+                println!("{}", log(Log::ServerClosedConnection));
                 break;
             }
+            resp => println!("{}", log(resp)),
         }
     }
 }
